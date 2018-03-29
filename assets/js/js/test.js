5207f4d01a530ffc21b4a77e11ff45df764ff754
stampstrong <37840212+stampstrong@users.noreply.github.com>
2018-03-29 20:56:19 +0700

    Version4

--- assets/js/js/test.js
+++ assets/js/js/test.js
@@ -1,38 +1,41 @@
 //current energy consumption update picture
-function update_member_profit(status) {
-    $("#member_profit").text(String(status));
-}
-function update_orders(number) {
-    $("#orders").text(String(number));
-}
-function update_ipo_margin(number1) {
-    $("#ipo_margin").text(String(number1));
-}
-function update_Issue_Reports(number2) {
-    $("#Issue_Reports").text(String(number2));
-}
-function update_Payments(number3) {
-    $("#Payments").text(String(number3));
-}
-function update_Logistics(number4) {
-    $("#Logistics").text(String(number4));
-}
-function update_Weekly_Orders(number5) {
-    $("#Weekly_Orders").text(String(number5));
-}
-function update_Transactions(number6) {
-    $("#Transactions").text(String(number6));
-}
-function update_Revenue(number7) {
-    $("#Revenue").text(String(number7));
+function update_data(id, number) {
+    $("#"+String(id)).text(String(number));
 }
 
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + String(Math.floor(Math.random()*10))).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(500)
+                    .height(400);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
 $( document ).ready(function() {
     console.log("starting document!!!!");
-
+    $(function ($) {
+             $("#firebasebutton").click(function (evt) {
+               console.log("firebasebutton was clicked")
+               writeUserData('warodom', 'warodom khamphanchai',
+               'kwarodom@vt.edu', 'https://pbs.twimg.com/profile_images/966896631147765760/AJ836huS_400x400.jpg')
+             })
+         });
     // Initialize Firebase
     console.log("Initialize Firebase");
+    // Initialize Firebase
     var config = {
       apiKey: "AIzaSyA914GyOh2srowW5MKf4LjKYnn44o1yDpo",
       authDomain: "python-trainning-91676.firebaseapp.com",
@@ -43,16 +46,7 @@ $( document ).ready(function() {
     };
     firebase.initializeApp(config);
 
-    // var ref = firebase.database().ref();
-    //
-    // ref.on("value", function(snapshot) {
-    //     console.log(snapshot.val().test);
-    //     x = snapshot.val().test;
-    // }, function (error) {
-    //     console.log("Error: " + error.code);
-    // });
-
-  //  total_load_activePower = 0;
+    var ref = firebase.database().ref();
 
     var member_profitRef = firebase.database().ref("member");
 
@@ -61,27 +55,9 @@ $( document ).ready(function() {
         console.log(data.val());
         if(data.key == "1PV221445K1200100") {
             total_load_activePower  = data.val().load_activePower;
-        } else if (data.key == 'member_profit') {
-            update_member_profit(parseInt(data.val()));
-        } else if (data.key == 'orders') {
-            update_orders(parseInt(data.val()));
-        } else if (data.key == 'ipo_margin') {
-              update_ipo_margin(parseInt(data.val()));
-        } else if (data.key == 'Issue_Reports') {
-                  update_Issue_Reports(parseInt(data.val()));
-        } else if (data.key == 'Payments') {
-                  update_Payments(parseInt(data.val()));
-        } else if (data.key == 'Logistics') {
-                  update_Logistics(parseInt(data.val()));
-        } else if (data.key == 'Weekly_Orders') {
-                  update_Weekly_Orders(parseInt(data.val()));
-        } else if (data.key == 'Transactions') {
-                  update_Transactions(parseInt(data.val()));
-        } else if (data.key == 'Revenue') {
-                  update_Revenue(parseInt(data.val()));
         } else {
+           update_data(data.key, parseInt(data.val()))
 
-            console.log("need to parse this key " + data.key)
         }
     });
 
